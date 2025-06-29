8bdc351807e22389b620e1868d26ac3a1565daba
Crespi <danielecr09@gmail.com>
2025-06-29 12:45:40 +0200

    Update auth.controller.ts

--- esercitazione_DDX/src/auth/auth.controller.ts
+++ esercitazione_DDX/src/auth/auth.controller.ts
@@ -1,27 +1,32 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { AuthDto, TokenDto} from "src/auth/dto";
+import { AuthDto, TokenDto } from "src/auth/dto";
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService){}
+export class AuthController {
+    constructor(private authService: AuthService) { }
 
     @Post('signup')
-    signup(@Body() dto : AuthDto){
+    signup(@Body() dto: AuthDto) {
         //console.log(dto); // debug
         return this.authService.signup(dto);
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('signin')
-    signin(@Body() dto : AuthDto){
+    signin(@Body() dto: AuthDto) {
         console.log(dto); // debug
         return this.authService.signin(dto);
     }
 
     @Post('refreshToken')
-    updateJWT(@Body() dto : TokenDto){
+    updateJWT(@Body() dto: TokenDto) {
         console.log(dto);
         return this.authService.refreshTokens(dto.refreshToken);
+    }
+
+    @Get('/test')
+    testAPI() {
+        return 'la chiamata API FUNZIONAAAAA';
     }
 }

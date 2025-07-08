8ad9a86edc2bbd64cc789900c9e21a570a1418f2
Crespi <72547251+Crespi09@users.noreply.github.com>
2025-07-08 11:46:23 +0200

    storage api

--- esercitazione_DDX/dist/storage/storage.service.js
+++ esercitazione_DDX/dist/storage/storage.service.js
@@ -17,29 +17,19 @@ let StorageService = class StorageService {
         this.prisma = prisma;
     }
     async userStorage(user) {
-        var storageUsed = 0;
-        const items = await this.prisma.item.findMany({
+        const result = await this.prisma.file.aggregate({
+            _sum: {
+                storage: true,
+            },
             where: {
-                owner: {
-                    is: {
-                        id: user.id
-                    }
-                }
-            }
+                item: {
+                    ownerId: user.id,
+                },
+            },
         });
-        await Promise.all(items.map(async (element) => {
-            const file = await this.prisma.file.findUnique({
-                where: {
-                    itemId: element.id
-                }
-            });
-            if (file) {
-                storageUsed += file.storage;
-            }
-        }));
         return {
             message: 'Total Storage used',
-            storage: storageUsed,
+            storage: result._sum.storage,
         };
     }
 };
--- esercitazione_DDX/src/storage/storage.service.ts
+++ esercitazione_DDX/src/storage/storage.service.ts
@@ -1,4 +1,9 @@
-import { ForbiddenException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
@@ -6,45 +11,23 @@ import { elementAt } from 'rxjs';
 
 @Injectable({})
 export class StorageService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
+  async userStorage(user: User) {
+    const result = await this.prisma.file.aggregate({
+      _sum: {
+        storage: true,
+      },
+      where: {
+        item: {
+          ownerId: user.id,
+        },
+      },
+    });
 
-    // TODO - per adesso non calcola il peso dei file che sono stati condivisi
-    async userStorage(user: User) {
-        /*
-            vado a prendere gli id degli item con ownserId = user.id
-            poi controllo la lista degli id nei file e prendo lo storage
-
-        */
-
-        var storageUsed = 0;
-
-        const items = await this.prisma.item.findMany({
-            where: {
-                owner: {
-                    is: {
-                        id: user.id
-                    }
-                }
-            }
-        });
-
-        await Promise.all(items.map(async (element) => {
-            const file = await this.prisma.file.findUnique({
-                where: {
-                    itemId: element.id
-                }
-            });
-
-            if (file) {
-                storageUsed += file.storage;
-            }
-        }));
-
-        return {
-            message: 'Total Storage used',
-            storage: storageUsed,
-        };
-    }
-
+    return {
+      message: 'Total Storage used',
+      storage: result._sum.storage,
+    };
+  }
 }
